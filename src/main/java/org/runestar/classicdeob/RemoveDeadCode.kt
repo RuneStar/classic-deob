57431ac4a89581312447b66a113d657a64158153
hunterwb <hunter@hunterwb.com>
2019-10-11 20:45:20 -0700

    Cleanup RemoveDeadCode

--- src/main/java/org/runestar/classicdeob/RemoveDeadCode.kt
+++ src/main/java/org/runestar/classicdeob/RemoveDeadCode.kt
@@ -9,21 +9,14 @@ object RemoveDeadCode : Transformer.Single() {
 
     override fun transform(klass: ClassNode) {
         for (m in klass.methods) {
-            var changed = true // todo
-            while (changed) {
-                changed = false
-                try {
-                    val frames = Analyzer(BasicInterpreter()).analyze(klass.name, m)
-                    val insns = m.instructions.toArray()
-                    for (i in frames.indices) {
-                        if (frames[i] == null) {
-                            changed = true
-                            m.instructions.remove(insns[i])
-                        }
-                    }
-                } catch (e: Exception) {
-                    throw Exception("${klass.name}.${m.name}${m.desc}", e)
+            try {
+                val frames = Analyzer(BasicInterpreter()).analyze(klass.name, m)
+                val insns = m.instructions.toArray()
+                for (i in frames.indices) {
+                    if (frames[i] == null) m.instructions.remove(insns[i])
                 }
+            } catch (e: Exception) {
+                throw Exception("${klass.name}.${m.name}${m.desc}", e)
             }
         }
     }
